8431e93735d106587f3e318334d4c4d569f9c40f
xbones84 <42823347+xbones84@users.noreply.github.com>
2018-09-11 19:48:00 -0500

    Multi-thread Framework

---
+++ MT_Framework.ps1
@@ -0,0 +1,78 @@
+$TimeToComplete = Measure-Command {
+
+$Parameters = @{
+    Param1 = 'Param1'
+    Param2 = 'Param2'
+}
+
+ 
+
+#$SessionState = [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault()
+
+$RunspacePool = [runspacefactory]::CreateRunspacePool(
+    1,#Min Runspaces
+    10000 #Max Runspaces
+)
+
+$RunspacePool.Open()
+
+$jobs = New-Object System.Collections.ArrayList
+
+1..50 | ForEach-Object { 
+    $PowerShell = [powershell]::Create()
+    $PowerShell.RunspacePool = $RunspacePool
+    [void]$PowerShell.AddScript({
+        Param (
+            $Param1,
+            $Param2
+        )
+
+        $ThreadID = [appdomain]::GetCurrentThreadId()
+
+        Write-Verbose "ThreadID: Beginning $ThreadID" -Verbose
+        $Sleep = Get-Random (1..5)
+
+        [pscustomobject]@{
+            Param1 = $param1
+            Param2 = $param2
+            Thread = $ThreadID
+            ProcessID = $PID
+            SleepTime = $Sleep
+        }
+        Start-Sleep -Seconds $Sleep
+    })
+
+    [void]$PowerShell.AddParameters($Parameters)
+
+    $Handle = $PowerShell.BeginInvoke()
+    $Temp = '' | Select-Object PowerShell, Handle
+
+    $Temp.PowerShell = $PowerShell
+    $Temp.Handle = $Handle
+    [void]$Jobs.add($Temp)
+
+    Write-Debug ("Available Runspaces in RunspacePool: {0}" -f $RunspacePool.GetAvailableRunspaces())
+    Write-Debug ("Remaining Jobs: {0}" -f @($jobs | Where-Object {
+        $_.handle.iscompleted -ne 'Completed'
+    }).Count)
+}
+
+ 
+
+$Return = $Jobs | ForEach-Object {
+    $_.PowerShell.EndInvoke($_.handle)
+    $_.PowerShell.Dispose()
+}
+
+}
+
+$Jobs.Clear()
+$Return |  Group ProcessID | Select Count, Name
+$Return | Group Thread | Select Count, Name
+($Return).Count
+$TimeToComplete
+
+ 
+
+
+
